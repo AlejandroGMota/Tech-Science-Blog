d25a80f511b1e411f67d2027dfac48b4e59c5793
AlejandroGMota <cmke14y@gmail.com>
2026-03-21 00:06:39 -0600

    feat: mobile responsive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
 import HomePage from './pages/HomePage/HomePage'
 import EntradasPage from './pages/EntradasPage/EntradasPage'
 import ArticlePage from './pages/ArticlePage/ArticlePage'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => { window.scrollTo(0, 0) }, [pathname])
+  return null
+}
+
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Navbar />
       <main>
         <Routes>
